85b797eeedc852e9e871fb01495ad1dc11783940
akshay9070 <86796423+akshay9070@users.noreply.github.com>
2021-07-08 23:31:11 +0530

    Ternary Operator C++

---
+++ Programs/ternary_operator.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int marks;
+    cout <<"Enter Your Marks : \n";
+
+    cin >>marks;
+    
+    marks>=45?cout << "Passed": cout << "Failed";
+
+    return 0;
+}
