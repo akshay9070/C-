7e655d97d16afceed911bfe80105637ca6924769
akshay9070 <86796423+akshay9070@users.noreply.github.com>
2021-07-09 00:04:58 +0530

    do while program in c++

---
+++ Programs/do_while.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+   int input;
+   do
+   {
+       cout <<"Welcome to my githib profile...\n";
+       cout <<"This is C++ Program....\n";
+       cout << "Enter 0 to exit or any other key to see this message..\n";
+       cin >> input;   
+   }while(input!=0);
+   return 0;
+
+}
