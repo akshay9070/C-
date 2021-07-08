449c98c63ab5c086e0ae97b032c69134bf676ffd
akshay9070 <86796423+akshay9070@users.noreply.github.com>
2021-07-08 21:00:02 +0530

    Arithmetic Operations Using C++

---
+++ Programs/Arithmetic Operations.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,add,sub,mul,div;
+
+    cout <<"Enter two number for arithmetic  operation : ";
+
+    cin >> a >> b;
+
+    cout <<"a = "<< a << "\nb = " << b << endl;
+    add = a + b;
+    sub = a-b;
+    mul = a*b;
+    div = a/b;
+
+    cout << a << "+" << b << " = "<< add << endl;
+    cout << a << "-" << b << " = "<< sub << endl;
+    cout << a << "*" << b << " = "<< mul << endl;
+    cout << a << "/" << b << " = "<<div << endl;
+     
+}
